ebc47e8bfb886344260e3d7aa97da692ca3b284f
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2021-02-01 10:30:40 +0800

    refactor: allow passing multiple domain names

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,12 @@ use std::sync::Arc;
 use anyhow::Context;
 use chrono::{DateTime, SubsecRound, TimeZone, Utc};
 use clap::{App, Arg, SubCommand};
+use crossbeam::thread;
 use num_format::{Locale, ToFormattedString};
 use rustls::{ClientConfig, Session};
 use serde_derive::{Deserialize, Serialize};
 use std::fmt;
-use x509_parser::der_parser::nom::lib::std::fmt::{Display, Formatter};
+use std::fmt::{Display, Formatter};
 use x509_parser::parse_x509_certificate;
 
 const CHECK: &'static str = "check";
@@ -42,18 +43,26 @@ fn main() -> anyhow::Result<()> {
         .get_matches();
 
     if let Some(ref m) = matches.subcommand_matches(CHECK) {
-        let domain_name = m.value_of(DOMAIN_NAME).expect("Domain name is not given");
-        let client = CheckClient::new();
-        match client.check_certificate(domain_name) {
-            Ok(r) => {
-                if matches.is_present(JSON) {
-                    let s = serde_json::to_string(&r.to_json())?;
-                    println!("{0}", s);
-                } else {
-                    println!("{0}", r);
-                }
+        let domain_names: Vec<String> = m
+            .values_of(DOMAIN_NAME)
+            .expect("Domain name is not given")
+            .map(String::from)
+            .collect();
+        let results = CheckClient::check_certificates(&domain_names)?;
+        if matches.is_present(JSON) {
+            if results.len() > 1 {
+                let json: Vec<CheckResultJSON> = results.iter().map(|r| r.to_json()).collect();
+                let s = serde_json::to_string(&json)?;
+                println!("{0}", s);
+            } else {
+                let json = results.get(0).unwrap().to_json();
+                let s = serde_json::to_string(&json)?;
+                println!("{0}", s);
             }
-            Err(e) => println!("{:?}", e),
+        } else {
+            for r in results {
+                println!("{0}", r);
+            }
         }
     }
 
@@ -109,19 +118,40 @@ impl CheckClient {
             checked_at,
             days: duration.num_days(),
             domain_name: domain_name.to_string(),
+            expired_at: not_after.to_rfc3339(),
             not_after,
-            seconds: duration.num_seconds(),
         })
+    }
+
+    fn check_certificates<S: AsRef<str>>(domain_names: &[S]) -> anyhow::Result<Vec<CheckResult>> {
+        let client = Arc::new(CheckClient::new());
+
+        thread::scope(|s| {
+            let mut handles = vec![];
+            for domain_name in domain_names {
+                let domain_name = domain_name.as_ref();
+                let client = client.clone();
+                handles.push(s.spawn(move |_| client.check_certificate(&domain_name)));
+            }
+
+            let mut results = vec![];
+            for handle in handles {
+                results.push(handle.join().unwrap()?);
+            }
+
+            Ok(results)
+        })
+        .unwrap()
     }
 
     fn build_http_headers(domain_name: &str) -> String {
         format!(
             concat!(
-            "GET / HTTP/1.1\r\n",
-            "Host: {0}\r\n",
-            "Connection: close\r\n",
-            "Accept-Encoding: identity\r\n",
-            "\r\n"
+                "GET / HTTP/1.1\r\n",
+                "Host: {0}\r\n",
+                "Connection: close\r\n",
+                "Accept-Encoding: identity\r\n",
+                "\r\n"
             ),
             domain_name
         )
@@ -131,11 +161,11 @@ impl CheckClient {
 #[derive(Debug)]
 struct CheckResult {
     ok: bool,
+    checked_at: DateTime<Utc>,
     days: i64,
     domain_name: String,
-    checked_at: DateTime<Utc>,
+    expired_at: String,
     not_after: DateTime<Utc>,
-    seconds: i64,
 }
 
 impl Display for CheckResult {
@@ -154,9 +184,9 @@ impl Display for CheckResult {
 
         if self.ok {
             s.push(format!(
-                "expires in {0} days ({1} seconds)",
+                "expires in {0} days ({1})",
                 self.days.to_formatted_string(&Locale::en),
-                self.seconds.to_formatted_string(&Locale::en)
+                self.expired_at
             ));
         } else {
             s.push(format!("is expired"));
@@ -169,10 +199,10 @@ impl Display for CheckResult {
 #[derive(Serialize, Deserialize)]
 struct CheckResultJSON {
     ok: bool,
+    checked_at: String,
     days: i64,
     domain_name: String,
-    checked_at: String,
-    seconds: i64,
+    expired_at: String,
 }
 
 impl CheckResult {
@@ -182,8 +212,8 @@ impl CheckResult {
             checked_at,
             domain_name: domain_name.to_string(),
             days: 0,
+            expired_at: "".to_string(),
             not_after: Utc.timestamp(0, 0),
-            seconds: 0,
         }
     }
 
@@ -193,7 +223,7 @@ impl CheckResult {
             days: self.days,
             domain_name: self.domain_name.clone(),
             checked_at: self.checked_at.to_rfc3339(),
-            seconds: self.seconds,
+            expired_at: self.expired_at.clone(),
         }
     }
 }
@@ -229,5 +259,14 @@ mod test {
         assert!(!resp.ok);
         assert!(checked_at_is_positive(&resp.checked_at));
         assert_eq!(0, resp.not_after.timestamp());
+    }
+
+    #[test]
+    fn test_check_certificates() {
+        let domain_names = vec!["sha512.badssl.com", "expired.badssl.com"];
+        let results = CheckClient::check_certificates(domain_names).unwrap();
+        assert_eq!(2, results.len());
+        assert!(results.get(0).unwrap().ok);
+        assert!(!results.get(1).unwrap().ok);
     }
 }
