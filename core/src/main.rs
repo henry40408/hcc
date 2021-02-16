579fc5267b76c4de23c1968acdd3073bf7275dff
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2021-02-17 01:22:07 +0800

    refactor: reduce fields and more tests

--- core/src/main.rs
+++ core/src/main.rs
@@ -1,6 +1,6 @@
 use structopt::StructOpt;
 
-use potential_giggle::{CheckClient, CheckResultsJSON};
+use potential_giggle::{CheckClient, CheckResultJSON};
 
 #[derive(Debug, StructOpt)]
 #[structopt(author, about)]
@@ -29,32 +29,64 @@ enum Command {
 
 fn main() -> anyhow::Result<()> {
     let opts: Opts = Opts::from_args();
-
     match opts.command {
         Some(Command::Check {
-            domain_names,
+            ref domain_names,
             grace_in_days,
-        }) => {
-            let client = CheckClient::new_with_grace_in_days(grace_in_days);
-            let results = client.check_certificates(domain_names.as_slice())?;
-            if opts.json {
-                if results.len() > 1 {
-                    let json: CheckResultsJSON = results.iter().map(|r| r.to_json()).collect();
-                    let s = serde_json::to_string(&json)?;
-                    println!("{0}", s);
-                } else {
-                    let json = results.get(0).unwrap().to_json();
-                    let s = serde_json::to_string(&json)?;
-                    println!("{0}", s);
-                }
-            } else {
-                for r in results {
-                    println!("{0}", r);
-                }
-            }
+        }) => check_command(opts.json, domain_names, grace_in_days),
+        None => Ok(()),
+    }
+}
+
+fn check_command<S: AsRef<str>>(
+    json: bool,
+    domain_names: &Vec<S>,
+    grace_in_days: i64,
+) -> anyhow::Result<()> {
+    let client = CheckClient::new_with_grace_in_days(grace_in_days);
+
+    let results = client.check_certificates(domain_names.as_slice())?;
+
+    if json {
+        let s = if results.len() > 1 {
+            let json: Vec<CheckResultJSON> = results.iter().map(CheckResultJSON::new).collect();
+            serde_json::to_string(&json)?
+        } else {
+            let result = results.get(0).unwrap();
+            let json = CheckResultJSON::new(result);
+            serde_json::to_string(&json)?
+        };
+        println!("{0}", s);
+    } else {
+        for r in results {
+            println!("{0}", r);
         }
-        None => {}
     }
 
     Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use crate::check_command;
+
+    #[test]
+    fn test_check_command() {
+        check_command(false, &vec!["sha512.badssl.com"], 7).unwrap();
+    }
+
+    #[test]
+    fn test_check_command_json() {
+        check_command(true, &vec!["sha512.badssl.com"], 7).unwrap();
+    }
+
+    #[test]
+    fn test_check_command_expired() {
+        check_command(false, &vec!["expired.badssl.com"], 7).unwrap();
+    }
+
+    #[test]
+    fn test_check_command_expired_json() {
+        check_command(true, &vec!["expired.badssl.com"], 7).unwrap();
+    }
 }
--- server/src/server.rs
+++ server/src/server.rs
@@ -1,7 +1,6 @@
 use actix_web::{get, web, Responder};
-use serde::{Deserialize, Serialize};
-
-use potential_giggle::CheckClient;
+use potential_giggle::{CheckClient, CheckResultJSON};
+use serde_json::json;
 
 pub struct SharedState {
     client: CheckClient,
@@ -15,22 +14,21 @@ impl SharedState {
     }
 }
 
-#[derive(Serialize, Deserialize)]
-struct ErrorJSON {
-    error: String,
-}
-
 #[get("/{domain_name}")]
 pub async fn show_domain_name(
     data: web::Data<SharedState>,
     web::Path(domain_name): web::Path<String>,
 ) -> impl Responder {
     let client = &data.client;
-    match client.check_certificate(&domain_name) {
-        Ok(r) => serde_json::to_string(&r.to_json()).expect("unable to serialize JSON"),
-        Err(e) => serde_json::to_string(&ErrorJSON {
-            error: format!("{:?}", e),
-        })
-        .expect("unable to serialize JSON"),
-    }
+    let string = match client.check_certificate(&domain_name) {
+        Ok(r) => {
+            let json = CheckResultJSON::new(&r);
+            serde_json::to_string(&json)
+        }
+        Err(e) => {
+            let json = json!({ "error": format!("{:?}", e) });
+            serde_json::to_string(&json)
+        }
+    };
+    string.expect("unable to serialize JSON")
 }
